c6104fc27bf64981decd1dcef4e2de33611fad66
snowtoslow <snowtoslow@gmail.com>
2021-03-24 18:10:52 +0200

    add client side streaming rpc

--- second-module/client/order-info.go
+++ second-module/client/order-info.go
@@ -21,6 +21,7 @@ func main() {
 
 	orderMgtClient := orderinfo.NewOrderManagementClient(conn)
 
+	/////unary rpc example;
 	retrievedOrder, err := orderMgtClient.GetOrder(context.Background(),
 		&wrappers.StringValue{Value: "order1"})
 	if err != nil {
@@ -28,7 +29,7 @@ func main() {
 	}
 	log.Print("GetOrder Response -> : ", retrievedOrder)
 
-
+	////Streaming server side rpc example;
 	searchStream, _ := orderMgtClient.SearchOrders(context.Background(),
 		&wrappers.StringValue{Value: "google"})
 
@@ -40,6 +41,41 @@ func main() {
 		// обрабатываем другие потенциальные ошибки
 		log.Print("Search Result : ", searchOrder)
 	}
+
+
+	/////Streaming client rpc example;
+	updateStream, err := orderMgtClient.UpdateOrders(context.Background())
+	if err != nil {
+		log.Fatalf("%v.UpdateOrders(_) = _, %v", orderMgtClient, err)
+	}
+
+	if err = updateStream.Send(&orderinfo.Order{
+		Id: "order1",
+		Items: []string{"vova is testing update!"},
+	}); err != nil {
+		log.Fatalf("%v.Send() =%v", updateStream, err)
+	}
+	// обновляем заказ 2
+	if err = updateStream.Send(&orderinfo.Order{
+		Id: "order2",
+		Items: []string{"myMagicRpc!"},
+	}); err != nil {
+		log.Fatalf("%v.Send() = %v", updateStream, err)
+	}
+	// обновляем заказ 3
+	if err = updateStream.Send(&orderinfo.Order{
+		Id: "order3",
+		Items: []string{"stream 3"},
+	}); err != nil {
+		log.Fatalf("%v.Send() = %v", updateStream, err)
+	}
+
+	updateRes, err := updateStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v, want %v",
+			updateStream, err, nil)
+	}
+	log.Printf("Update Orders Res : %s", updateRes)
 
 
 }
